649817313f472d619a9e8236306016f1bdf6bd8f
stassyn <alexskripkoit@gmail.com>
2015-11-05 11:54:43 +0200

    0.12 Alpha

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Maxim Papezhuk',
     author_email = 'maxp.job@gmail.com',
     url = 'https://github.com/Max201/cleave',
-    download_url = 'https://github.com/Max201/cleave/tarball/v0.1',
+    download_url = 'https://github.com/Max201/cleave/tarball/v0.12',
     keywords = ['python', 'server', 'simple'],
     classifiers = [],
 )
