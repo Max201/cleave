f7d18f8fd26a9a837275215862b8a1b3ffe0db49
stassyn <alexskripkoit@gmail.com>
2015-11-05 11:46:49 +0200

    Pypi repository setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'cleave',
+    packages = ['cleave'],
+    version = '0.1',
+    description = 'Python easy server library',
+    author = 'Maxim Papezhuk',
+    author_email = 'maxp.job@gmail.com',
+    url = 'https://github.com/Max201/cleave',
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+    classifiers = [],
+)
