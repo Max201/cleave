7215fb544bf01a3c8f7476599b7d4948cfc2dc82
stassyn <alexskripkoit@gmail.com>
2016-03-17 10:32:22 +0200

    Tarball update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='cleave',
     packages=['cleave'],
-    version='0.16',
+    version='0.17',
     description='Python easy tools library',
     author='Maxim Papezhuk',
     author_email='maxp.job@gmail.com',
     url='https://github.com/Max201/cleave',
-    download_url='https://github.com/Max201/cleave/tarball/v0.15',
+    download_url='https://github.com/Max201/cleave/tarball/v0.17',
     keywords=['server', 'python', 'socket', 'easy', 'encryption'],
     classifiers=[],
     entry_points={
