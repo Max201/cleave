aafbd79e8ba7bc766503eeabc02e77d655500fb4
stassyn <alexskripkoit@gmail.com>
2016-03-17 11:10:43 +0200

    Fixed base file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name='cleave',
     packages=find_packages(),
-    version='0.23',
+    version='0.24',
     description='Python easy tools library. Allows to run local http server in one command.',
     author='Maxim Papezhuk',
     author_email='maxp.job@gmail.com',
     url='https://github.com/Max201/cleave',
-    download_url='https://github.com/Max201/cleave/tarball/v0.23',
+    download_url='https://github.com/Max201/cleave/tarball/v0.24',
     keywords=['server', 'python', 'socket', 'easy', 'encryption', 'http server', 'file server'],
     classifiers=[],
     entry_points={
