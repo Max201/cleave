9857eef6f8c0b8a9e31aae20a393e5a890758f83
stassyn <alexskripkoit@gmail.com>
2016-03-17 16:47:57 +0200

    Updated pypi settings

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name='cleave',
     packages=find_packages(),
-    version='0.26',
+    version='0.27',
     description='Python easy tools library. Allows to run local http server in one command.',
     author='Maxim Papezhuk',
     author_email='maxp.job@gmail.com',
     url='https://github.com/Max201/cleave',
-    download_url='https://github.com/Max201/cleave/tarball/v0.26',
+    download_url='https://github.com/Max201/cleave/tarball/v0.27',
     keywords=['server', 'python', 'socket', 'easy', 'encryption', 'http server', 'file server'],
     classifiers=[],
     entry_points={
